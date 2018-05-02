a75ad09e4d73339ca3a555c58028f97f9eea3b15
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-05-02 18:11:55 +0200

    s

---
+++ environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAKX38Y1K8SlyFGPwBLlG5Nnx0ldHJKhM0",
+    authDomain: "x7-causal-root-p.firebaseapp.com",
+    databaseURL: "https://x7-causal-root-p.firebaseio.com",
+    projectId: "x7-causal-root-p",
+    storageBucket: "",
+    messagingSenderId: "835156018956"
+  }
+};
---
+++ environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAKX38Y1K8SlyFGPwBLlG5Nnx0ldHJKhM0",
+    authDomain: "x7-causal-root-p.firebaseapp.com",
+    databaseURL: "https://x7-causal-root-p.firebaseio.com",
+    projectId: "x7-causal-root-p",
+    storageBucket: "",
+    messagingSenderId: "835156018956"
+  }
+};
